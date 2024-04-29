b4fe6c0e7bb76e3d1f0efb40a53f5e3c6d9db52f
ShahwaizZahid <shahwaizmughal940@gmail.com>
2024-04-30 01:08:35 +0500

    fix: gitignore .env file

--- server/index.js
+++ server/index.js
@@ -1,27 +1,28 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const urlRoute = require("./routes/url.js");
+
 const { connectToMongo } = require("./connect.js");
+
 const URL = require("./models/url.js");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
+
 const MONGO = process.env.MONGO;
+
 app.use(express.json());
+app.use(cors());
+
 const {
   handleGenerateNewUrl,
   handleGetAnalytics,
   handleRequestUrl,
-
 } = require("./controller/url.js");
 
-
-connectToMongo(
-MONGO
-).then(() => {
+connectToMongo(MONGO).then(() => {
   console.log("Mongo DB successfully connected !");
 });
-app.use(cors());
 
 const PORT = process.env.PORT;
 
